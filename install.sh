4cf85ebe9bc0c43b71d8dd041977d1b9abce637e
tozzemon <tozzemon@gmail.com>
2024-11-18 01:53:44 +0600

    .

--- install.sh
+++ install.sh
@@ -1,66 +1,64 @@
 #! /bin/bash
 
-echo -e "\nread -n 1 -p \"One key away from the miracle: \" && echo\n"
-sleep 0.5
 read -n 1 -p "One key away from the miracle: " && echo
 
-echo -e "\nmkfs.btrfs -f /dev/sda3\n"
+echo -e "\nEXECUTION: mkfs.btrfs -fq /dev/sda3\n"
 sleep 0.5
-mkfs.btrfs -f /dev/sda3
+mkfs.btrfs -fq /dev/sda3
 
-echo -e "\nmount /dev/sda3 /mnt\n"
+echo -e "\nEXECUTION: mount /dev/sda3 /mnt\n"
 sleep 0.5
 mount /dev/sda3 /mnt
 
-echo -e "\nbtrfs subvolume create /mnt/@\n"
+echo -e "\nEXECUTION: btrfs subvolume create /mnt/@\n"
 sleep 0.5
 btrfs subvolume create /mnt/@
 
-echo -e "\nbtrfs subvolume create /mnt/@snapshots\n"
+echo -e "\nEXECUTION: btrfs subvolume create /mnt/@snapshots\n"
 sleep 0.5
 btrfs subvolume create /mnt/@snapshots
 
-echo -e "\numount /mnt\n"
+echo -e "\nEXECUTION: umount /mnt\n"
 sleep 0.5
 umount /mnt
 
-echo -e "\nmount -o subvol=@ /dev/sda3 /mnt\n"
+echo -e "\nEXECUTION: mount -o subvol=@ /dev/sda3 /mnt\n"
 sleep 0.5
 mount -o subvol=@ /dev/sda3 /mnt
 
-echo -e "\nmkfs.ext4 /dev/sda2\n"
+echo -e "\nEXECUTION: mkfs.ext4 -q /dev/sda2\n"
 sleep 0.5
-mkfs.ext4 /dev/sda2
+mkfs.ext4 -q /dev/sda2
 
-echo -e "\nmkfs.vfat /dev/sda1\n"
+echo -e "\nEXECUTION: mkfs.vfat -q /dev/sda1\n"
 sleep 0.5
-mkfs.vfat /dev/sda1
+mkfs.vfat -q /dev/sda1
 
-echo -e "\nmount --mkdir /dev/sda2 /mnt/boot\n"
+echo -e "\nEXECUTION: mount --mkdir /dev/sda2 /mnt/boot\n"
 sleep 0.5
 mount --mkdir /dev/sda2 /mnt/boot
 
-echo -e "\ndinitctl start ntpd\n"
+echo -e "\nEXECUTION: dinitctl start ntpd\n"
 sleep 0.5
 dinitctl start ntpd
 
-echo -e "\npacman -Sy --noconfirm\n"
+echo -e "\nEXECUTION: pacman -Sy --noconfirm\n"
 sleep 0.5
 pacman -Sy --noconfirm
 
-echo -e "\nbasestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit\n"
+echo -e "\nEXECUTION: basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit\n"
 sleep 0.5
 basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit
 
-echo -e "\nfstabgen -U /mnt >> /mnt/etc/fstab\n"
+echo -e "\nEXECUTION: fstabgen -U /mnt >> /mnt/etc/fstab\n"
 sleep 0.5
 fstabgen -U /mnt >> /mnt/etc/fstab
 
-echo -e "\ncat ./install2 > /mnt/root/.bashrc\n"
+echo -e "\nEXECUTION: cat ./install2 > /mnt/root/.bashrc\n"
 sleep 0.5
 cat ./install2 > /mnt/root/.bashrc
 
-echo -e "\nartix-chroot /mnt\n"
+echo -e "\nEXECUTION: artix-chroot /mnt\n"
 sleep 0.5
 artix-chroot /mnt
 
