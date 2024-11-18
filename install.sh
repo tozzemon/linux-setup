4edf34f366b6615dc9ea2cfbb6dcdeca98cf9124
tozzemon <tozzemon@gmail.com>
2024-11-18 07:00:07 +0600

    .

--- install.sh
+++ install.sh
@@ -5,80 +5,80 @@ echo && read -n 1 -p "One key away from the miracle... " && echo
 # Set BTRFS
 
 echo -e "\nEXECUTION: mkfs.btrfs -fq /dev/sda3\n"
-sleep 0.5
+read
 mkfs.btrfs -fq /dev/sda3
 
 echo -e "\nEXECUTION: mount /dev/sda3 /mnt\n"
-sleep 0.5
+read
 mount /dev/sda3 /mnt
 
 echo -e "\nEXECUTION: btrfs subvolume create /mnt/@\n"
-sleep 0.5
+read
 btrfs subvolume create /mnt/@
 
 echo -e "\nEXECUTION: btrfs subvolume create /mnt/@snapshots\n"
-sleep 0.5
+read
 btrfs subvolume create /mnt/@snapshots
 
 echo -e "\nEXECUTION: umount /mnt\n"
-sleep 0.5
+read
 umount /mnt
 
 echo -e "\nEXECUTION: mount -o subvol=@ /dev/sda3 /mnt\n"
-sleep 0.5
+read
 mount -o subvol=@ /dev/sda3 /mnt
 
 # Format sda1, sda2
 
 echo -e "\nEXECUTION: mkfs.ext4 -fq /dev/sda2\n"
-sleep 0.5
+read
 mkfs.ext4 -fq /dev/sda2
 
 echo -e "\nEXECUTION: mkfs.vfat -fq /dev/sda1\n"
-sleep 0.5
+read
 mkfs.vfat -fq /dev/sda1
 
 # Mount /boot
 
 echo -e "\nEXECUTION: mount --mkdir /dev/sda1 /mnt/boot\n"
-sleep 0.5
+read
 mount --mkdir /dev/sda1 /mnt/boot
 
 # Enable ntpd
 
 echo -e "\nEXECUTION: dinitctl start ntpd\n"
-sleep 0.5
+read
 dinitctl start ntpd
 
 # Install packages
 
 echo -e "\nEXECUTION: pacman -Sy --noconfirm\n"
-sleep 0.5
+read
 pacman -Sy --noconfirm
 
 echo -e "\nEXECUTION: basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit\n"
-sleep 0.5
+read
 basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit
 
 # Generate Fstab
 
 echo -e "\nEXECUTION: fstabgen -U /mnt >> /mnt/etc/fstab\n"
-sleep 0.5
+read
 fstabgen -U /mnt >> /mnt/etc/fstab
 
 # Prepare to chroot
 
 echo -e "\nEXECUTION: cat ./install2.sh > /mnt/root/.bashrc\n"
-sleep 0.5
+read
 cat ./install2.sh > /mnt/root/.bashrc
 
 echo -e "\nEXECUTION: cp -r ./dots /mnt/root && cp -r ./configs /mnt/root\n"
-sleep 0.5
+read
 cp -r ./dots /mnt/root && cp -r ./configs /mnt/root
 
 # chroot
 
 echo -e "\nEXECUTION: artix-chroot /mnt\n"
-sleep 0.5
+read
 artix-chroot /mnt
 
--- install2.sh
+++ install2.sh
@@ -1,87 +1,87 @@
 echo -e "\nEXECUTION: ln -sf /usr/share/zoneinfo/Asia/Bishkek /etc/localtime\n"
-sleep 0.5
+read
 ln -sf /usr/share/zoneinfo/Asia/Bishkek /etc/localtime
 
 echo -e "\nEXECUTION: hwclock --systohc\n"
-sleep 0.5
+read
 hwclock --systohc
 
 echo -e "\nEXECUTION: echo \"en_US.UTF-8 UTF-8\" > /etc/locale.gen\n"
-sleep 0.5
+read
 echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 
 echo -e "\nEXECUTION: echo \"LANG=en_US.UTF-8\" > /etc/locale.conf\n"
-sleep 0.5
+read
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
 echo -e "\nEXECUTION: locale-gen\n"
-sleep 0.5
+read
 locale-gen
 
 echo -e "\nEXECUTION: grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB\n"
-sleep 0.5
+read
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 
 echo -e "\nEXECUTION: grub-mkconfig -o /boot/grub/grub.cfg\n"
-sleep 0.5
+read
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\nEXECUTION: echo \"desktop\" > /etc/hostname\n"
-sleep 0.5
+read
 echo "desktop" > /etc/hostname
 
 echo -e "\nEXECUTION: echo \"root:2486\" | chpasswd\n"
-sleep 0.5
+read
 echo "root:2486" | chpasswd
 
 echo -e "\nEXECUTION: useradd -m -G wheel -s /bin/bash tozzemon\n"
-sleep 0.5
+read
 useradd -m -G wheel -s /bin/bash tozzemon
 
 echo -e "\nEXECUTION: echo \"tozzemon:2486\" | chpasswd\n"
-sleep 0.5
+read
 echo "tozzemon:2486" | chpasswd
 
 echo -e "\nEXECUTION: sed -i \"/^# %wheel ALL=(ALL) ALL/s/^# //\" /etc/sudoers\n"
-sleep 0.5
+read
 sed -i "/^# %wheel ALL=(ALL) ALL/s/^# //" /etc/sudoers
 
 # echo -e "\nEXECUTION: cd /opt && git clone https://aur.archlinux.org/yay.git && chown -R \$(whoami):\$(whoami) yay && cd yay && makepkg -si\n"
-# sleep 0.5
+# read
 # cd /opt && git clone https://aur.archlinux.org/yay.git && chown -R $(whoami):$(whoami) yay && cd yay && makepkg -si
 
 echo -e "\nEXECUTION: mkdir -p ~/.config && cp -r ./dots/* /home/tozzemon/.config\n"
-sleep 0.5
+read
 mkdir -p ~/.config && cp -r /root/dots/* /home/tozzemon/.config
 
 echo -e "\nEXECUTION: pacman -S --noconfirm nvidia-open nvidia-settings\n"
-sleep 0.5
+read
 pacman -S --noconfirm nvidia-open nvidia-settings
 
 echo -e "\nEXECUTION: sed -i 's/\\<kms\\>//g; s/  / /g; s/ ( /(/; s/ ) /)/' /etc/mkinitcpio.conf\n"
-sleep 0.5
+read
 sed -i 's/\<kms\>//g; s/  / /g; s/ ( /(/; s/ ) /)/' /etc/mkinitcpio.conf
 
 echo -e "\nEXECUTION: mkdir -p /etc/X11 && cp ./configs/xorg.conf /etc/X11/\n"
-sleep 0.5
+read
 mkdir -p /etc/X11 && cp /root/configs/xorg.conf /etc/X11/
 
 # echo -e "\nEXECUTION: yay -S xbanish librewolf-bin intellij-idea-community-edition-jre\n"
-# sleep 0.5
+# read
 # yay -S xbanish librewolf-bin intellij-idea-community-edition-jre
 
 echo -e "\nEXECUTION: dinitctl enable connmand\n"
-sleep 0.5
+read
 dinitctl enable connmand
 
 echo -e "\nEXECUTION: dinitctl enable ly\n"
-sleep 0.5
+read
 dinitctl enable ly
 
 echo -e "\nEXECUTION: rm /root/.bashrc && rm -rf /root/configs && rm -rf /root/dots\n"
-sleep 0.5
+read
 rm /root/.bashrc && rm -rf /root/configs && rm -rf /root/dots
 
 echo -e "\nEXECUTION: exit\n"
-sleep 0.5
+read
 exit
