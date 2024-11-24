62c9e3884d8b68ab1d0b7c61bfcc444b6c06b384
tozzemon <tozzemon@gmail.com>
2024-11-24 06:11:17 +0600

    .

---
+++ .install.sh
@@ -0,0 +1,102 @@
+#! /bin/bash
+# Live-CD
+
+# ROOT_PATH=""
+# BOOT_PATH=""
+# SWAP_PATH=""
+# TIMEZONE=""
+# LOCALE=""
+# ROOT_PASS=""
+# USERNAME=""
+# USER_PASS=""
+# 
+# read -n 1 -p "'Enter' to proceed with defaults\n[C]onfigure installation\n[U]pdate\n[Q]uit"
+# if [[ "$INPUT" == "" ]]; then
+# 	default-installation
+# elif [[ "$INPUT" == "C" || "$INPUT" == "c" ]]; then
+# 	configure-installation
+# elif [[ "$INPUT" == "U" || "$INPUT" == "u" ]]; then
+# 	update
+# elif [[ "$INPUT" == "Q" || "$INPUT" == "q" ]]; then
+# 	quit
+# else
+# 	echo -e "\n! No such option\n"
+# fi
+
+echo && read -n 1 -p "One key away from the miracle... " && echo
+
+# Set BTRFS
+
+echo -e "\nEXECUTION: mkfs.btrfs -fq /dev/sda3\n"
+mkfs.btrfs -fq /dev/sda3
+
+echo -e "\nEXECUTION: mount /dev/sda3 /mnt\n"
+mount /dev/sda3 /mnt
+
+echo -e "\nEXECUTION: btrfs subvolume create /mnt/@\n"
+btrfs subvolume create /mnt/@
+
+echo -e "\nEXECUTION: btrfs subvolume create /mnt/@snapshots\n"
+btrfs subvolume create /mnt/@snapshots
+
+echo -e "\nEXECUTION: umount /mnt\n"
+umount /mnt
+
+echo -e "\nEXECUTION: mount -o subvol=@ /dev/sda3 /mnt\n"
+mount -o subvol=@ /dev/sda3 /mnt
+
+# Format sda1, sda2
+
+echo -e "\nEXECUTION: mkfs.ext4 -Fq /dev/sda2\n"
+mkfs.ext4 -Fq /dev/sda2
+
+echo -e "\nEXECUTION: mkfs.vfat /dev/sda1\n"
+mkfs.vfat /dev/sda1
+
+# Mount /boot
+
+echo -e "\nEXECUTION: mount --mkdir /dev/sda1 /mnt/boot\n"
+mount --mkdir /dev/sda1 /mnt/boot
+
+# Enable ntpd
+
+echo -e "\nEXECUTION: dinitctl start ntpd\n"
+dinitctl start ntpd
+
+# Install packages
+
+echo -e "\nEXECUTION: pacman -Sy --noconfirm\n"
+pacman -Sy --noconfirm
+
+echo -e "\nEXECUTION: basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit\n"
+basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit
+
+# Generate Fstab
+
+echo -e "\nEXECUTION: fstabgen -U /mnt >> /mnt/etc/fstab\n"
+fstabgen -U /mnt >> /mnt/etc/fstab
+
+# Add install2.sh to .bashrc in the installed system
+
+echo -e "\nEXECUTION: cat ./install2.sh > /mnt/root/.bashrc\n"
+cat ./install2.sh > /mnt/root/.bashrc
+
+# Add dots and configs to /root in the installed system
+
+echo -e "\nEXECUTION: cp -r ./dots /mnt/root && cp -r ./configs /mnt/root\n"
+cp -r ./dots /mnt/root && cp -r ./configs /mnt/root
+
+# Write install3.sh to /home/tozzemon/.bash_profile in the installed system
+
+echo -e "\nEXECUTION: mkdir -p /mnt/home/tozzemon && cat ./install3.sh > /mnt/home/tozzemon/.bash_profile\n"
+mkdir -p /mnt/home/tozzemon && cat ./install3.sh > /mnt/home/tozzemon/.bash_profile
+
+# Chroot
+
+echo -e "\nEXECUTION: artix-chroot /mnt\n"
+artix-chroot /mnt
+
+# Reboot
+
+echo -e "\nEXECUTION: reboot\n"
+reboot
--- install.sh
+++ install.sh
@@ -1,102 +1,281 @@
 #! /bin/bash
-# Live-CD
 
-# ROOT_PATH=""
-# BOOT_PATH=""
-# SWAP_PATH=""
-# TIMEZONE=""
-# LOCALE=""
-# ROOT_PASS=""
-# USERNAME=""
-# USER_PASS=""
-# 
-# read -n 1 -p "'Enter' to proceed with defaults\n[C]onfigure installation\n[U]pdate\n[Q]uit"
-# if [[ "$INPUT" == "" ]]; then
-# 	default-installation
-# elif [[ "$INPUT" == "C" || "$INPUT" == "c" ]]; then
-# 	configure-installation
-# elif [[ "$INPUT" == "U" || "$INPUT" == "u" ]]; then
-# 	update
-# elif [[ "$INPUT" == "Q" || "$INPUT" == "q" ]]; then
-# 	quit
-# else
-# 	echo -e "\n! No such option\n"
-# fi
 
-echo && read -n 1 -p "One key away from the miracle... " && echo
 
-# Set BTRFS
+# ---------------------------------------
+# FLAGS
+# ---------------------------------------
 
-echo -e "\nEXECUTION: mkfs.btrfs -fq /dev/sda3\n"
-mkfs.btrfs -fq /dev/sda3
 
-echo -e "\nEXECUTION: mount /dev/sda3 /mnt\n"
-mount /dev/sda3 /mnt
 
-echo -e "\nEXECUTION: btrfs subvolume create /mnt/@\n"
-btrfs subvolume create /mnt/@
+# `confirm` (type: bool, default: false) - toggles confirmation message before each command execution;
+# `delay` (type: bool, default: false) - adds one second delay after each command.
 
-echo -e "\nEXECUTION: btrfs subvolume create /mnt/@snapshots\n"
-btrfs subvolume create /mnt/@snapshots
 
-echo -e "\nEXECUTION: umount /mnt\n"
-umount /mnt
 
-echo -e "\nEXECUTION: mount -o subvol=@ /dev/sda3 /mnt\n"
-mount -o subvol=@ /dev/sda3 /mnt
+# ---------------------------------------
+# GLOBAL VARIABLES
+# ---------------------------------------
 
-# Format sda1, sda2
 
-echo -e "\nEXECUTION: mkfs.ext4 -Fq /dev/sda2\n"
-mkfs.ext4 -Fq /dev/sda2
 
-echo -e "\nEXECUTION: mkfs.vfat /dev/sda1\n"
-mkfs.vfat /dev/sda1
+# Store numbers of the current stage and command for more clarity in the case of debugging
+command_number=1
+stage_number=1
 
-# Mount /boot
+# Stores commands for execution and has to be used in pair with the `x()` method in the `scenarion()`
+c=""
 
-echo -e "\nEXECUTION: mount --mkdir /dev/sda1 /mnt/boot\n"
-mount --mkdir /dev/sda1 /mnt/boot
+# Just a fancy thing to have
+prfx="[Script]"
 
-# Enable ntpd
+# Default kernel (the script supposes separately kernel specifying)
+basestrap_default_kernel="linux" 
 
-echo -e "\nEXECUTION: dinitctl start ntpd\n"
-dinitctl start ntpd
+# Default package set
+basestrap_default_packages="linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit"
 
-# Install packages
+# Minimal package set
+basestrap_minimal_packages="linux-firmware base dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit bash-completion grub gvim neofetch neovim ntfs-3g openssh wget"
 
-echo -e "\nEXECUTION: pacman -Sy --noconfirm\n"
-pacman -Sy --noconfirm
+# The current kernel to install
+basestrap_kernel="$basestrap_default_kernel"
 
-echo -e "\nEXECUTION: basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit\n"
-basestrap /mnt linux linux-firmware base base-devel dinit elogind-dinit os-prober efibootmgr connman-dinit xorg xorg-xinit ly-dinit git alacritty bash-completion beep breeze-gtk breeze5 bspwm btrfs-progs dunst grub gvim lxappearance neofetch neovim ntfs-3g openssh qt5ct rofi sxhkd unclutter viewnior vifm wget xorg xorg-xinit
+# The current set of packages to install
+basestrap_packages="$basestrap_default_packages"
 
-# Generate Fstab
 
-echo -e "\nEXECUTION: fstabgen -U /mnt >> /mnt/etc/fstab\n"
-fstabgen -U /mnt >> /mnt/etc/fstab
 
-# Add install2.sh to .bashrc in the installed system
+# ---------------------------------------
+# METHODS
+# ---------------------------------------
 
-echo -e "\nEXECUTION: cat ./install2.sh > /mnt/root/.bashrc\n"
-cat ./install2.sh > /mnt/root/.bashrc
 
-# Add dots and configs to /root in the installed system
 
-echo -e "\nEXECUTION: cp -r ./dots /mnt/root && cp -r ./configs /mnt/root\n"
-cp -r ./dots /mnt/root && cp -r ./configs /mnt/root
+# Greeter
 
-# Write install3.sh to /home/tozzemon/.bash_profile in the installed system
+grtr() {
+	echo -e "\n-------------------------------------------------"
+	echo -e "Artix Dinit Dummy Installation Script by tozzemon"
+	echo -e "-------------------------------------------------"
+	menu
+}
 
-echo -e "\nEXECUTION: mkdir -p /mnt/home/tozzemon && cat ./install3.sh > /mnt/home/tozzemon/.bash_profile\n"
-mkdir -p /mnt/home/tozzemon && cat ./install3.sh > /mnt/home/tozzemon/.bash_profile
+# Main menu
 
-# Chroot
+menu() {
+	local input=""
+	echo -e "\nInstallation options\n"
+	echo -e " [S]tart immediately\n [B]asestrap adjustment\n [U]pdate the script\n [H]Help\n [Q]uit"
+	
+	while [ true ]; do
 
-echo -e "\nEXECUTION: artix-chroot /mnt\n"
-artix-chroot /mnt
+		echo && read -n 1 -p ">>> " input && echo
+		
+		case "$input" in
 
-# Reboot
+			[Ss]) scenario ;;
+			[Bb]) basestrapManager ;;
+			[Uu]) update ;;
+			[Qq]) exit ;;
+			*) echo -e "\n ! There's no such option..." ;;
 
-echo -e "\nEXECUTION: reboot\n"
-reboot
+		esac
+
+	done
+}
+
+# Command executor
+
+x() {
+	local input=""
+	local pass=false
+
+        echo -e "\nStage: $stage_number Step: $command_number Command: $c"
+
+	if [ "$confirm" == true ]; then
+
+		while [ "$pass" == false ]; do
+
+			read -n 1 -p "Proceed? [Y/n] " input && echo
+
+			case "$input" in
+				
+				[Yy]|"") pass=true ;;
+				[Nn]) echo "Aborted."; exit ;;
+				*) continue ;;
+
+			esac
+
+		done
+
+	fi
+
+	pass=false
+	eval "$c"
+	
+	if [ "$delay" == true ]; then
+		sleep 1
+	fi
+
+	((counter++))
+}
+
+# Basestrap command's manager
+
+basestrapManager() {
+	local input=""
+
+	while [ true ]; do
+		echo -e "\nWhat to do with basestrap?\n"
+		echo -e " [F]ull redefine\n [K]ernel redefine\n [M]inimal system set\n [D]efault system set\n [S]how current set\n [B]ack"
+		echo && read -n 1 -p ">>> " input && echo 
+
+		case "$input" in
+
+			[Ff])
+				echo && read -p "Type in a kernel to use (specifies separately): " basestrap_kernel ;
+				echo && read -p "Type in other packages names: " basestrap_packages ;
+				echo -e "\nPackages successfully updated." ;;
+			[Kk])
+				echo && read -p "Specify a new kernel: " basestrap_kernel ;
+				echo -e "\nKernel package successfully updated." ;;
+			[Mm])
+				echo -e "\nThe minimal package set is:\n\nKernel: $basestrap_default_kernel\n\nPackages: $basestrap_minimal_packages\n" ;
+				read -n 1 -p "Apply it? [Y/n] " input && echo ;
+
+				case "$input" in
+					
+					[Yy]|"")
+						basestrap_kernel="$basestrap_default_kernel" ;
+						basestrap_packages="$basestrap_minimal_packages" ;
+						echo -e "\nApplied." ;
+						basestrapManager ;;
+					[Nn])
+						echo -e "\nCanceled.";
+						basestrapManager ;;
+					*) continue ;;
+
+				esac ;;
+			[Dd]) 	
+				echo -e "\nThe default package set is:\n\nKernel: $basestrap_default_kernel\n\nPackages: $basestrap_default_packages\n" ;
+				read -n 1 -p "Apply it? [Y/n] " input && echo ;
+
+				case "$input" in
+					
+					[Yy]|"")
+						basestrap_kernel="$basestrap_default_kernel" ;
+						basestrap_packages="$basestrap_default_packages" ;
+						echo -e "\nApplied." ;
+						basestrapManager ;;
+					[Nn])
+						echo -e "\nCanceled.";
+						basestrapManager ;;
+					*) continue ;;
+
+				esac ;;
+			[Ss]) 
+				echo -e "\nThe current package set is:\n\nKernel:\n$basestrap_kernel\n\nPackages:\n$basestrap_packages" ;;
+			[Bb]) menu;;
+
+		esac
+
+	done
+}
+
+# The script's updator, tries to suck up a new version from Github
+
+update() {
+	git pull && echo - "\nRelaunching the script...\n" && exec ./script.sh
+}
+
+# Installation scenario
+
+scenario() {
+	# Paths
+	
+	local boot_path="/dev/sda1"
+	local swap_path="/dev/sda2"
+	local root_path="/dev/sda3"
+	local boot_dir="/mnt/boot"
+
+	# Setting partitions
+	
+	c="mkfs.btrfs -fq $root_path" && x
+	c="mount $root_path /mnt" && x
+	c="btrfs subvolume create /mnt/@" && x
+	c="btrfs subvolume create /mnt/@snapshots" && x
+	c="umount /mnt" && x
+	c="mount -o subvol=@ $root_path /mnt" && x
+	c="mkfs.ext4 -Fq $swap_path" && x
+	c="mkfs.vfat $boot_path" && x
+	c="mount --mkdir $boot_path $boot_dir" && x
+	
+	# Configuring network
+	
+	c="dinitctl start ntpd" && x
+	
+	# Updating package database
+	
+	c="pacman -Sy --noconfirm" && x
+	
+	# Installing packages
+	
+	c="basestrap /mnt $basestrap_kernel $basestrap_packages" && x
+	
+	# Generating Fstab
+	
+	c="fstabgen -U /mnt >> /mnt/etc/fstab" && x
+	
+	# Creating a directory for configuration files
+	
+	c="mkdir -p /mnt/configurations" && x
+	
+	# Copying configurations
+	
+	c="cp -r ./configurations/dots /mnt/configurations && cp -r ./configurations/xorg /mnt/configurations && cp -r ./configurations/themes /mnt/configurations" && x
+
+	# Overriding `/mnt/root/.bashrc` content with the script's instance
+
+	c="cat ./install.sh > /mnt/root/.bashrc" && x
+
+	# Updating the stage number
+
+	((stage_number++))
+
+	# Saving temporary settings
+	
+	c="cat <<EOF > ./var.tmp
+stage_number="$stage_number"
+basestrap_kernel="$basestrap_kernel"
+basestrap_packages="$basestrap_packages"
+EOF" && x
+
+	# Copying temporary settings to `/mnt/root`
+	
+	c="cp ./var.tmp /mnt/root" && x
+
+	# Chroot into the installed system
+	
+	c="artix-chroot /mnt" && x
+
+	# Reboot
+	
+	c="reboot" && x
+
+}
+
+grtr
+
+
+
+
+
+
+
+
+
+# SDDM
+
+# Add SDDM theme from ./themes/where_is_my_sddm_theme to /usr/share/sddm/themes
+# Add backing up /usr/share/sddm/scripts/Xsetup to turn off the second monitor
